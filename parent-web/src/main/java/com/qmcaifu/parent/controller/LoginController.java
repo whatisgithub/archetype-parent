5f1f28defdaadd4743cc5e68fee8ff205a8eca56
woya <woya2010@126.com>
2016-09-14 13:47:40 +0800

    编译小error

--- parent-web/src/main/java/com/qmcaifu/parent/controller/LoginController.java
+++ parent-web/src/main/java/com/qmcaifu/parent/controller/LoginController.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * Created by Administrator on 2016/8/11.
  */
